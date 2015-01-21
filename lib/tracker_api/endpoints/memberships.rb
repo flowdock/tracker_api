56c73c2d05d3a8633a344dfff4cccf008c3de6d4
sakari <sakariij@gmail.com>
2015-01-21 11:25:25 +0200

    get project member

--- lib/tracker_api/endpoints/memberships.rb
+++ lib/tracker_api/endpoints/memberships.rb
@@ -7,6 +7,11 @@ module TrackerApi
         @client = client
       end
 
+      def get_member(project_id, user_id)
+        data = client.get("/projects/#{project_id}/memberships/#{user_id}").body
+        Resources::ProjectMembership.new({ client: client, project_id: project_id }.merge(data))
+      end
+
       def get(project_id, params={})
         data = client.paginate("/projects/#{project_id}/memberships", params: params)
         raise TrackerApi::Errors::UnexpectedData, 'Array of memberships expected' unless data.is_a? Array
